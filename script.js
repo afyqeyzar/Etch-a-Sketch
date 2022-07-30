4f35bcd8abb32f958c854279bee36f3fa138353f
afyqeyzar <norafyqe@gmail.com>
2022-07-30 13:41:45 +0100

    added first square to grid

---
+++ script.js
@@ -0,0 +1,9 @@
+const container = document.querySelector('#container');
+
+const div = document.createElement('div');
+
+div.setAttribute('style', 'background: black; height: 50px; width: 50px; margin: 10px;');
+//div.textContent = "test"
+
+
+
