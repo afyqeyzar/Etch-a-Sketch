886d96282dcf95053ad0f8174316c93b7f5a4f66
afyqeyzar <norafyqe@gmail.com>
2022-07-30 13:59:33 +0100

    added loop to append divs into container

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const container = document.querySelector('#container');
 
-const div = document.createElement('div');
+for (let i = 0; i < 16*16; i++){
+    const div = document.createElement('div');
+    div.setAttribute('style', 'background: black; height: 20px; width: 20px; margin: 1px; padding; 0px;');
+    container.append(div)
+}
 
-div.setAttribute('style', 'background: black; height: 50px; width: 50px; margin: 10px;');
-//div.textContent = "test"
+
+//setting styles
 
 
-container.appendChild(div)
+
+
+
+
+
+
