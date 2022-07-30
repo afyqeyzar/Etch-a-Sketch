43fa1260cef8f094c4354db6856af54d95d16aaf
afyqeyzar <norafyqe@gmail.com>
2022-07-30 19:48:11 +0100

    when mouse over box console.log('test')

--- script.js
+++ script.js
@@ -3,15 +3,21 @@ const container = document.querySelector('#container');
 for (let i = 0; i < 16*16; i++){
     const div = document.createElement('div');
     div.classList.add('box');
-    container.append(div)
+    container.append(div);
+}
+
+//const hover = Array.from(document.querySelectorAll('.box'));
+
+function changeColor(){
+    div.style.cssText = 'background: white;';
 }
 
 
-//setting styles
 
-
-
-
+const hover = Array.from(document.querySelectorAll('.box'));
+  hover.forEach(box => box.addEventListener('mouseover', (e) => {
+    console.log(e)
+  }));
 
 
 
