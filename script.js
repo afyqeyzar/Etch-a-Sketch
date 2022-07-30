006c368e04ac758520ff382634d2deeb86fb4bae
afyqeyzar <norafyqe@gmail.com>
2022-07-30 17:37:47 +0100

    16 by 16 grid now up using display: grid;

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const container = document.querySelector('#container');
 
 for (let i = 0; i < 16*16; i++){
     const div = document.createElement('div');
-    div.setAttribute('style', 'background: black; height: 20px; width: 20px; margin: 1px; padding; 0px;');
+    div.classList.add('box');
     container.append(div)
 }
 
