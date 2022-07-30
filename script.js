f5c0988415b945b0380758cac4fbd75e53400b9d
afyqeyzar <norafyqe@gmail.com>
2022-07-30 20:17:10 +0100

    mousing over will change the whole grid to black

--- script.js
+++ script.js
@@ -9,15 +9,15 @@ for (let i = 0; i < 16*16; i++){
 //const hover = Array.from(document.querySelectorAll('.box'));
 
 function changeColor(){
-    div.style.cssText = 'background: white;';
+    const div = Array.from(document.querySelectorAll('.box'));
+    div.forEach(box => box.classList.remove('color-box')); 
+    div.forEach(box => box.classList.add('color-box')); 
 }
 
 
 
 const hover = Array.from(document.querySelectorAll('.box'));
-  hover.forEach(box => box.addEventListener('mouseover', (e) => {
-    console.log(e)
-  }));
+  hover.forEach(box => box.addEventListener('mouseover', changeColor));
 
 
 
