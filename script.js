d86dece8aad20454eabae89e0ef16c3e6143fd99
afyqeyzar <norafyqe@gmail.com>
2022-08-01 08:48:01 +0100

    tried to add a button to produce different grids but does not work yet

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const container = document.querySelector('#container');
+//sixteenBtn = document.querySelector('#sixteen');
+//sixteenBtn.addEventListener('click', sixteen);
 
-const ROW_WIDTH = 16
+const ROW_WIDTH = 16;
 
-for (let i = 0; i < ROW_WIDTH ** 2; i++){
-    const div = document.createElement('div');
-    div.classList.add('box');
-    container.append(div);
+function sixteen() {
+    //document.getElementById("sixteen").disabled = true;
+    for (let i = 0; i < ROW_WIDTH ** 2; i++){
+        const div = document.createElement('div');
+        div.classList.add('box');
+        container.append(div);
+    }
 }
+
+sixteen()
+
 
 const hover = Array.from(document.querySelectorAll('.box'));
 hover.forEach(box => box.addEventListener('mouseover', () => {
