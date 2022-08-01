0a7cb545d52abd743d6a638070fcfafe308a8ec9
afyqeyzar <norafyqe@gmail.com>
2022-08-01 07:12:00 +0100

    finally got it to display properly

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
 const container = document.querySelector('#container');
 
-for (let i = 0; i < 16*16; i++){
+const ROW_WIDTH = 16
+
+for (let i = 0; i < ROW_WIDTH ** 2; i++){
     const div = document.createElement('div');
     div.classList.add('box');
     container.append(div);
 }
 
-//const hover = Array.from(document.querySelectorAll('.box'));
-
-function changeColor(){
-    const div = Array.from(document.querySelectorAll('.box'));
-    div.forEach(box => box.classList.remove('color-box')); 
-    div.forEach(box => box.classList.add('color-box')); 
-}
-
-
-
 const hover = Array.from(document.querySelectorAll('.box'));
-  hover.forEach(box => box.addEventListener('mouseover', changeColor));
-
-
+hover.forEach(box => box.addEventListener('mouseover', () => {
+    box.classList.add('color-box')
+}));
 
 
